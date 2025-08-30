0ae206e8c166b087904654670eff3ad9819b18bb
Larson-j <lahcen.mou@web.de>
2025-08-30 15:05:50 +0200

    remove unneeded files

--- github-setup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Wrapper: moved to scripts/ci/github-setup.sh
-exec "$(dirname "$0")/scripts/ci/github-setup.sh" "$@"
--- integration-test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Wrapper: moved to scripts/tests/integration-test.sh
-exec "$(dirname "$0")/scripts/tests/integration-test.sh" "$@"
